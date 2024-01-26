a0bc15cebe21322e71f943d33cc1e1d593fc3250
Dalmas.Otieno <tutor.dalmas.otieno@gmail.com>
2024-01-26 12:45:08 +0300

    front end start

---
+++ src/pages/Questionare.js
@@ -0,0 +1,24 @@
+export default function Questionare(){
+    return (
+        <div className="">
+            {/* Question 1 */}
+            <label>Kind of exercise</label>
+            <select name="cars" id="cars" form="carform">
+                <option value="Running">Runnig</option>
+                <option value="Walking">Walking</option>
+            </select><br />
+            {/* Question 2 */}
+            <label>Elevation or Climb in meters</label>
+            <input type="number" placeholder="0"/><br />
+            {/* Question 3 */}
+            <label>Duratioin of exercise in seconds</label>
+            <input type="number" placeholder="0"/><br />
+            {/* Question 3 */}
+            <label>Distance covered in meters</label>
+            <input type="number" placeholder="0"/><br />
+            {/* Question 3 */}
+            <label>Date and time when you took the exercise</label>
+            <input type="datetime-local" />
+        </div>
+    )
+}
